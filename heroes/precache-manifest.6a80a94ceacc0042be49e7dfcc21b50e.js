59f4de8bbf3b58bc67458c16c28d46175de85343
Tron Worlds <tronworlds@protonmail.com>
2020-07-07 15:14:27 +1000

    Battle heroes added

---
+++ heroes/precache-manifest.6a80a94ceacc0042be49e7dfcc21b50e.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "73b39e5a50b1c6abdebe6d5952d1c877",
+    "url": "/heroes/index.html"
+  },
+  {
+    "revision": "620173b651c69aa685dc",
+    "url": "/heroes/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "ab9cfc4b9516f08399a9",
+    "url": "/heroes/static/css/main.43d0e839.chunk.css"
+  },
+  {
+    "revision": "620173b651c69aa685dc",
+    "url": "/heroes/static/js/2.1cd96056.chunk.js"
+  },
+  {
+    "revision": "c6d4def1d96c62150ce6396c6f23d335",
+    "url": "/heroes/static/js/2.1cd96056.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ab9cfc4b9516f08399a9",
+    "url": "/heroes/static/js/main.e6b0311e.chunk.js"
+  },
+  {
+    "revision": "9faa1c4c8bfeccb52bb3",
+    "url": "/heroes/static/js/runtime-main.dd0ee618.js"
+  },
+  {
+    "revision": "8d4202fda214b4977d1e1802f438b2cf",
+    "url": "/heroes/static/media/Castle.8d4202fd.jpg"
+  },
+  {
+    "revision": "eb8a18708085fd38b78d9932eb0da14c",
+    "url": "/heroes/static/media/discord.eb8a1870.png"
+  },
+  {
+    "revision": "3963fa0ae78b915ee1fffac7913df9c9",
+    "url": "/heroes/static/media/logo.3963fa0a.png"
+  },
+  {
+    "revision": "d4d3f4e0ead46b1472c5104cee7fac59",
+    "url": "/heroes/static/media/telegram.d4d3f4e0.png"
+  }
+]);
